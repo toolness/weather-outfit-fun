fd7fbf8f6c7785b50dfaa4f74c12e51dcf0755f1
Atul Varma <varmaa@gmail.com>
2014-08-19 12:55:49 -0400

    added basic loading/start views.

--- src/main.js
+++ src/main.js
@@ -1,22 +1,52 @@
-$(function() {
-  var outfit = $('#outfit');
+var Router = Backbone.Router.extend({
+  routes: {
+    '': 'start',
+    'outfit': 'outfit'
+  },
+  setMainView: function(view) {
+    if (this.mainView)
+      this.mainView.remove();
+    this.mainView = view;
+    view.$el.appendTo('body');
+    return view;
+  },
+  start: function() {
+    this.setMainView(new StartView()).render();
+  },
+  outfit: function() {
+    this.setMainView(new OutfitView()).start();
+  }
+});
 
-  if (!outfit.length)
-    outfit = $('<div id="outfit"></div>').appendTo('body');
+var StartView = Backbone.View.extend({
+  render: function() {
+    this.$el.html(START_HTML);
+  }
+});
+
+var OutfitView = Backbone.View.extend({
+  start: function() {
+    this.$el.html(LOADING_HTML);
+    getCurrentPositionForecast(function(err, forecast) {
+      if (err)
+        return Template.render(this.$el, 'error-template', err);
+      Template.render(this.$el, 'outfit-template', {
+        city: forecast.city,
+        forecast: typeof(window.getForecastWords) == 'function'
+                  ? getForecastWords(forecast) : '???',
+        outfit: typeof(window.getForecastOutfit) == 'function'
+                ? getForecastOutfit(forecast) : null
+      });
+      console.log(forecast);
+    }.bind(this));
+  }
+});
+
+$(function() {
+  var router = new Router();
 
   Template.setDefault('outfit-template', OUTFIT_HTML);
   Template.setDefault('error-template', ERROR_HTML);
 
-  getCurrentPositionForecast(function(err, forecast) {
-    if (err)
-      return Template.render(outfit, 'error-template', err);
-    Template.render(outfit, 'outfit-template', {
-      city: forecast.city,
-      forecast: typeof(window.getForecastWords) == 'function'
-                ? getForecastWords(forecast) : '???',
-      outfit: typeof(window.getForecastOutfit) == 'function'
-              ? getForecastOutfit(forecast) : null
-    });
-    console.log(forecast);
-  });
+  Backbone.history.start();
 });
