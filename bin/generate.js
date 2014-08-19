268c88508e6b5342440ccb892d0c18e7ab74246c
Atul Varma <varmaa@gmail.com>
2014-08-19 09:25:43 -0400

    add support for source maps.

--- bin/generate.js
+++ bin/generate.js
@@ -1,26 +1,51 @@
 var fs = require('fs');
+var SourceMapGenerator = require('source-map').SourceMapGenerator;
 
 var SRC_DIR = __dirname + '/../src';
 var OUTPUT_FILENAME = 'weather-outfit.js';
 var OUTPUT_FILE = __dirname + '/../' + OUTPUT_FILENAME;
+var MAP_FILENAME = OUTPUT_FILENAME + '.map';
+var MAP_FILE = OUTPUT_FILE + '.map';
 
 function buildJs() {
-  function include(filename) {
+  var map = new SourceMapGenerator({
+    file: OUTPUT_FILENAME,
+    sourceRoot: '/src/'
+  });
+  var lines = ['//# sourceMappingURL=' + MAP_FILENAME];
+
+  function write(content, source) {
+    content.split('\n').forEach(function(line, i) {
+      if (source)
+        map.addMapping({
+          generated: {line: lines.length + 1, column: 0},
+          source: source,
+          original: {line: 1 + i, column: 0}
+        });
+      lines.push(line);
+    });
+  }
+
+  function readFile(filename) {
     return fs.readFileSync(SRC_DIR + '/' + filename, 'utf-8');
   }
 
-  function define(variable, filename) {
-    return 'var ' + variable + ' = ' +
-           JSON.stringify(include(filename)) + ';';
+  function include(filename) {
+    write(readFile(filename), filename);
   }
 
-  fs.writeFileSync(OUTPUT_FILE, [
-    '// This file was auto-generated, please do not edit it.',
-    define('OUTFIT_HTML', 'outfit.html'),
-    define('ERROR_HTML', 'error.html'),
-    include('mustache.js'),
-    include('weather-outfit-base.js')
-  ].join('\n\n'));
+  function define(variable, filename) {
+    write('var ' + variable + ' = ' +
+          JSON.stringify(readFile(filename)) + ';');
+  }
+
+  define('OUTFIT_HTML', 'outfit.html'),
+  define('ERROR_HTML', 'error.html'),
+  include('mustache.js'),
+  include('weather-outfit-base.js')
+
+  fs.writeFileSync(OUTPUT_FILE, lines.join('\n'));
+  fs.writeFileSync(MAP_FILE, map.toString());
 }
 
 module.exports = buildJs;
