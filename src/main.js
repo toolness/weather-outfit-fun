e6c2138fc09823273d770eecada33099b40945a7
Atul Varma <varmaa@gmail.com>
2014-08-26 16:02:09 -0400

    add dat-gui based debug panel if window.DEBUG is truthy.

--- src/main.js
+++ src/main.js
@@ -71,6 +71,8 @@ var OutfitView = Backbone.View.extend({
     this.$el.html(LOADING_HTML);
     var find = city ? getForecast.bind(null, city)
                     : getCurrentPositionForecast;
+    if (window.DEBUG && window.USE_FAKE_FORECAST)
+      find = function(cb) { cb(null, window.FAKE_FORECAST); };
     find(function(err, forecast) {
       if (err)
         return Template.render(this.$el, 'error-template', err);
@@ -103,8 +105,6 @@ var OutfitView = Backbone.View.extend({
 
       if (forecastOutfit instanceof Node || forecastOutfit instanceof $)
         this.$el.find('.forecastOutfit').empty().append(forecastOutfit);
-
-      console.log(forecast);
     }.bind(this));
   }
 });
@@ -133,12 +133,81 @@ window.getForecastWords = function getForecastWords(forecast) {
   return tempWords + ' and ' + forecast.weather;
 };
 
+window.enableDebugGUI = function() {
+  var gui = new dat.GUI();
+  var reload = function() {
+    Backbone.history.stop();
+    Backbone.history.start();
+/*    router.navigate(window.location.hash.slice(1), {
+      trigger: true,
+      replace: true
+    });*/
+  };
+
+  if (typeof(USE_FAKE_FORECAST) == 'undefined')
+    window.USE_FAKE_FORECAST = false;
+  if (typeof(FAKE_FORECAST) == 'undefined') {
+    window.FAKE_FORECAST = {
+      city: 'Fakeville',
+      date: new Date(),
+      humidity: 50,
+      weather: 'clear',
+      temp: {
+        f: 32,
+        c: 0
+      }
+    };
+  }
+
+  gui.add(window, 'USE_FAKE_FORECAST').onChange(function(value) {
+    if (value) {
+      forecast.open();
+      temp.open();
+    } else {
+      forecast.close();
+      temp.close();
+    }
+  });
+  gui.width = 300;
+
+  var forecast = gui.addFolder('FAKE_FORECAST');
+  var temp = forecast.addFolder('temp');
+
+  forecast.add(FAKE_FORECAST, 'city').onChange(reload);
+  forecast.add(FAKE_FORECAST, 'humidity', 0, 100).onChange(reload);
+  forecast.add(FAKE_FORECAST, 'weather', [
+    "clear",
+    "partly cloudy",
+    "mostly cloudy",
+    "raining",
+    "thunderstorming",
+    "snowing",
+    "misty"
+  ]).onChange(reload);
+  temp.add(FAKE_FORECAST.temp, 'f', -129, 134).listen()
+    .onChange(function(value) {
+      FAKE_FORECAST.temp.c = Math.round((value - 32) * 5/9);
+      reload();
+    });
+  temp.add(FAKE_FORECAST.temp, 'c', -89, 57).listen()
+    .onChange(function(value) {
+      FAKE_FORECAST.temp.f = Math.round(value * 9/5 + 32);
+      reload();
+    });
+
+  gui.open();
+  window.debugGUI = gui;
+};
+
 $(function() {
   Template.setDefault('outfit-template', OUTFIT_HTML);
   Template.setDefault('error-template', ERROR_HTML);
 
   if (!$('#app').length)
     $('<div id="app"></div>').appendTo('body');
+
+  if (window.DEBUG)
+    enableDebugGUI();
 
   Backbone.history.start();
   router.loadLastFragment();
