4afbb7df64196180b844a48c228377eb512ec506
Atul Varma <varmaa@gmail.com>
2014-09-05 12:24:29 -0400

    added challenge system to tutorial.

--- tutorial/common.js
+++ tutorial/common.js
@@ -1,3 +1,5 @@
+var PREFIX = 'weatherOutfit_';
+
 var baseURL = window.location.protocol + '//' + window.location.host +
               window.location.pathname;
 baseURL = baseURL.match(/^(.+\/)tutorial\/.*$/)[1];
