a2a5ab192e4b540aba16e2247e2c18aac4cf0ec2
Atul Varma <varmaa@gmail.com>
2014-09-05 12:28:52 -0400

    move getStorage/setStorage to tutorial/common.js.

--- tutorial/common.js
+++ tutorial/common.js
@@ -3,3 +3,17 @@ var PREFIX = 'weatherOutfit_';
 var baseURL = window.location.protocol + '//' + window.location.host +
               window.location.pathname;
 baseURL = baseURL.match(/^(.+\/)tutorial\/.*$/)[1];
+
+function getStorage(key, defaultValue) {
+  try {
+    return JSON.parse(sessionStorage[PREFIX + key]);
+  } catch (e) {
+    return defaultValue;
+  }
+}
+
+function setStorage(key, value) {
+  try {
+    sessionStorage[PREFIX + key] = JSON.stringify(value);
+  } catch (e) {}
+}
