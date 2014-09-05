4f82af29ce71134476b216efe2426094ef11de08
Atul Varma <varmaa@gmail.com>
2014-09-05 12:24:35 -0400

    rebuild files.

--- weather-outfit.js
+++ weather-outfit.js
@@ -16500,6 +16500,27 @@ window.addEventListener('error', function(event) {
     .prependTo('body');
 });
 
+(function() {
+  var me = document.scripts[document.scripts.length-1].src;
+
+  $(function() {
+    var CHALLENGE_POLL_INTERVAL = 1000;
+    var baseURL = me.match(/^(.+\/)weather-outfit\.js$/)[1];
+    var iframe = document.createElement('iframe');
+
+    iframe.setAttribute('src', baseURL + 'tutorial/challenges.html');
+
+    $('body').append(iframe);
+    $(iframe).hide();
+
+    setInterval(function() {
+      iframe.contentWindow.postMessage(JSON.stringify({
+        loaded: true
+      }), '*');
+    }, CHALLENGE_POLL_INTERVAL);
+  });
+})();
+
 var Router = Backbone.Router.extend({
   routes: {
     '': 'start',
