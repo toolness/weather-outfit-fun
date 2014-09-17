73193e77f8ed056ad38501f0a1b06406b4967f3a
Atul Varma <varmaa@gmail.com>
2014-09-17 16:25:17 -0400

    when templates are overridden, use their .html() instead of .text().

--- src/template.js
+++ src/template.js
@@ -1,6 +1,6 @@
 var Template = {
   render: function renderTemplate(target, filename, ctx) {
-    var template = $('[data-filename="' + filename + '"]').text() ||
+    var template = $('[data-filename="' + filename + '"]').html() ||
                    RAW_FILES[filename];
     target.html(Mustache.render(template, ctx));
   }
