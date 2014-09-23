dedf10fa2b95614cd5aa4498365251c9604aff4c
Atul Varma <varmaa@gmail.com>
2014-09-23 06:43:09 -0400

    Add a linkify button to the tutorial simulator.
    This enables quick sharing of tutorial simulator state.
    
    Note that because the current HTML is also shared, this enables
    reflected XSS on the origin of the tutorial simulator!

--- tutorial/common.js
+++ tutorial/common.js
@@ -17,3 +17,57 @@ function setStorage(key, value) {
     sessionStorage[PREFIX + key] = JSON.stringify(value);
   } catch (e) {}
 }
+
+function isStorageKey(key) { return key.indexOf(PREFIX) == 0; }
+
+// http://stackoverflow.com/a/2880929
+function parseQueryStringParams(query) {
+  if (typeof(query) == 'undefined')
+    query = window.location.search.substring(1);
+
+  var match;
+  var pl = /\+/g;  // Regex for replacing addition symbol with a space
+  var search = /([^&=]+)=?([^&]*)/g;
+  var decode = function (s) {
+    return decodeURIComponent(s.replace(pl, " "));
+  };
+
+  var urlParams = {};
+  while (match = search.exec(query))
+    urlParams[decode(match[1])] = decode(match[2]);
+  return urlParams;
+}
+
+function stringifyQueryStringParams(urlParams) {
+  return Object.keys(urlParams).map(function(key) {
+    return encodeURIComponent(key) + '=' +
+           encodeURIComponent(urlParams[key]);
+  }).join('&');
+}
+
+function embedStorageInCurrentURL() {
+  var urlParams = parseQueryStringParams();
+  Object.keys(sessionStorage).filter(isStorageKey)
+    .forEach(function(key) { urlParams[key] = sessionStorage[key]; });
+
+  return window.location.origin + window.location.pathname + '?' +
+         stringifyQueryStringParams(urlParams) + window.location.hash;
+}
+
+function restoreStorageFromCurrentURL() {
+  var urlParams = parseQueryStringParams();
+  var storageChanged = false;
+  Object.keys(urlParams).filter(isStorageKey)
+    .forEach(function(key) {
+      if (key in sessionStorage) return;
+      sessionStorage[key] = urlParams[key];
+      delete urlParams[key];
+      storageChanged = true;
+    });
+  if (storageChanged) {
+    window.history.replaceState(
+      {}, '',
+      '?' + stringifyQueryStringParams(urlParams) + window.location.hash
+    );
+  }
+}
